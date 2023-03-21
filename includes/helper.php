22cbfaa65aed26b75134bf956d3967094184442d
localhost8080 <localhost8080@gmail.com>
2023-03-21 12:54:09 +0000

    mods

--- includes/helper.php
+++ includes/helper.php
@@ -31,6 +31,7 @@ function get_carts_information()
     $result = $wpdb->get_results("SELECT * FROM $table_name");
 
     echo "<table>";
+    echo "<tr><td>Product</td><td>Quantity</td><td>Total</td></tr>";
     foreach ($result as $session) {
         // do we have actual cart datas?
         $datas = unserialize($session->session_value);
@@ -39,18 +40,18 @@ function get_carts_information()
 
             if (!empty($array)) {
                 //     // we have a cart
- 
+
                 $temp = array_keys($array);
                 $key = $temp[0];
                 $cart = $array[$key];
-
+                $product = wc_get_product($cart['product_id']);
                 echo "<tr>";
-                echo "<td>" . $cart['product_id'] . "</td>";
+                echo "<td>(" . $cart['product_id'] . ") " . $product->get_name() . "</td>";
                 echo "<td>" . $cart['quantity'] . "</td>";
                 echo "<td>" . $cart['line_total'] . "</td>";
                 echo "</tr>";
 
-              
+
 
                 // echo "<tr><td>" . $product->get_name() . "</td><td>" . $cart[$key_name[0]]['quantity'] . "</td></tr>";
 
