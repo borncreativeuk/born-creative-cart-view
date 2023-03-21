3fab7fe82683283cff6b6ce070c229c8d3de230f
localhost8080 <localhost8080@gmail.com>
2023-03-21 12:21:53 +0000

    moving

--- includes/helper.php
+++ includes/helper.php
@@ -1,6 +1,21 @@
 <?php
-
 // helper functions
+
+add_action('admin_menu', 'woocommerce_cart_view_admin_stuff', 99);
+
+function woocommerce_cart_view_admin_stuff(){
+	add_options_page("View Carts", "View Carts", "publish_pages", "View Carts", "woocommerce_cart_admin_view");
+}
+
+function woocommerce_cart_admin_view(){
+	// include admin view
+	if (file_exists(WP_PLUGIN_DIR . '/woocommerce-cart-view/views/woocommerce-cart-view-admin-view.php')) {
+		require_once WP_PLUGIN_DIR . '/woocommerce-cart-view/views/woocommerce-cart-view-admin-view.php';
+	}
+}
+
+
+
 
 
 function get_carts_information()
