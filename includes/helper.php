e4311751153786ad024342800531a863f2e662f9
localhost8080 <localhost8080@gmail.com>
2023-03-21 12:34:09 +0000

    mods

--- includes/helper.php
+++ includes/helper.php
@@ -3,15 +3,17 @@
 
 add_action('admin_menu', 'woocommerce_cart_view_admin_stuff', 99);
 
-function woocommerce_cart_view_admin_stuff(){
-	add_menu_page("View Carts", "View Carts", "publish_pages", "View Carts", "woocommerce_cart_admin_view");
+function woocommerce_cart_view_admin_stuff()
+{
+    add_menu_page("View Carts", "View Carts", "publish_pages", "View Carts", "woocommerce_cart_admin_view");
 }
 
-function woocommerce_cart_admin_view(){
-	// include admin view
-	if (file_exists(WP_PLUGIN_DIR . '/woocommerce-cart-view/views/woocommerce-cart-view-admin-view.php')) {
-		require_once WP_PLUGIN_DIR . '/woocommerce-cart-view/views/woocommerce-cart-view-admin-view.php';
-	}
+function woocommerce_cart_admin_view()
+{
+    // include admin view
+    if (file_exists(WP_PLUGIN_DIR . '/woocommerce-cart-view/views/woocommerce-cart-view-admin-view.php')) {
+        require_once WP_PLUGIN_DIR . '/woocommerce-cart-view/views/woocommerce-cart-view-admin-view.php';
+    }
 }
 
 
@@ -31,15 +33,16 @@ function get_carts_information()
     foreach ($result as $session) {
         // do we have actual cart datas?
         $datas = unserialize($session->session_value);
-        echo '<pre>';
-        print_r($datas);
-        echo '</pre>';
+        $cart = unserialize($datas->cart);
 
-        // $datas = unserialize($session->session_value);
-        // if (!empty($datas->cart)) {
-        //     // we have a cart
-        //     // show it
-        //     echo '<pre>' . print_r($datas->cart, true) . '</pre>';
-        // }
+
+        if (!empty($cart)) {
+            //     // we have a cart
+            //     // show it
+            //     echo '<pre>' . print_r($datas->cart, true) . '</pre>';
+            echo '<pre>';
+            print_r($cart);
+            echo '</pre>';
+        }
     }
 }
