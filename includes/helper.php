8d6819d14f3ee7fa56796e3e9490156931bf0b6e
localhost8080 <localhost8080@gmail.com>
2023-03-21 12:31:27 +0000

    mods

--- includes/helper.php
+++ includes/helper.php
@@ -28,17 +28,18 @@ function get_carts_information()
     $table_name = $wpdb->prefix . 'woocommerce_sessions';
     $result = $wpdb->get_results("SELECT * FROM $table_name");
 
-    echo '<pre>';
-    print_r($result);
-    echo '</pre>';
+    foreach ($result as $session) {
+        // do we have actual cart datas?
 
-    // foreach ($result as $session) {
-    //     // do we have actual cart datas?
-    //     $datas = unserialize($session->session_value);
-    //     if (!empty($datas->cart)) {
-    //         // we have a cart
-    //         // show it
-    //         echo '<pre>' . print_r($datas->cart, true) . '</pre>';
-    //     }
-    // }
+        echo '<pre>';
+        print_r($session->session_value);
+        echo '</pre>';
+
+        // $datas = unserialize($session->session_value);
+        // if (!empty($datas->cart)) {
+        //     // we have a cart
+        //     // show it
+        //     echo '<pre>' . print_r($datas->cart, true) . '</pre>';
+        // }
+    }
 }
