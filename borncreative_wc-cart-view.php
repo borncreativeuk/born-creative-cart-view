d261c193ebea226ccaaba5a933aaf2a5daefc617
localhost8080 <localhost8080@gmail.com>
2023-04-17 14:53:19 +0100

    mods

--- borncreative_wc-cart-view.php
+++ borncreative_wc-cart-view.php
@@ -1,6 +1,6 @@
 <?php
-  /*
-  Plugin name: Born Creative woocommerce cart view
+/*
+  Plugin name: Born Creative Cart View
   Plugin URI: https://github.com/localhost8080/woocommerce-cart-view
   Description: plugin to see cart items in woocommerce baskets
   Author: Born Creative
@@ -9,20 +9,20 @@
   License: GPL v3 or later
   License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
-  woocommerce cart view is free software: you can redistribute it and/or modify
+  Born Creative Cart View is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   any later version.
  
-  woocommerce cart view is distributed in the hope that it will be useful,
+  Born Creative Cart View is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   GNU General Public License for more details.
  
   You should have received a copy of the GNU General Public License
-  along with woocommerce cart view. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
+  along with Born Creative Cart View. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
   */
-  
+
 // helper functions
 if (file_exists(WP_PLUGIN_DIR . '/borncreative_wc-cart-view/includes/helper.php')) {
 	require_once WP_PLUGIN_DIR . '/borncreative_wc-cart-view/includes/helper.php';
@@ -32,5 +32,3 @@ if (file_exists(WP_PLUGIN_DIR . '/borncreative_wc-cart-view/includes/helper.php'
 if (file_exists(WP_PLUGIN_DIR . '/borncreative_wc-cart-view/includes/borncreative_wc-cart-view-admin.php')) {
 	require_once WP_PLUGIN_DIR . '/borncreative_wc-cart-view/includes/borncreative_wc-cart-view-admin.php';
 }
-
-
