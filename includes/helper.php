672516ad189392090784c6c3e758b49c1c0e1bae
localhost8080 <localhost8080@gmail.com>
2023-04-17 15:10:12 +0100

    mods

--- includes/helper.php
+++ includes/helper.php
@@ -5,72 +5,63 @@ add_action('admin_menu', 'borncreative_wc_cart_view_admin_stuff', 99);
 
 function borncreative_wc_cart_view_admin_stuff()
 {
-	add_menu_page(
-		"Born Creative",
-		"Born Creative",
-		"publish_pages",
-		"Born Creative",
+    add_menu_page(
+		"Born Carts",
+		"View Carts", 
+		"publish_pages", 
+		"View Carts", 
 		"borncreative_wc_cart_admin_view"
 	);
-	// Create a sub-menu under the top-level menu
-// 	add_submenu_page(
-// 		'born-creative',
-// 		'View Carts',
-// 		'View Carts',
-// 		'manage_options',
-// 		'view-carts',
-// 		'view_carts_menu_callback'
-// 	);
 }
 
 function borncreative_wc_cart_admin_view()
 {
-	// include admin view
-	if (file_exists(WP_PLUGIN_DIR . '/borncreative_wc-cart-view/views/borncreative_wc-cart-view-admin-view.php')) {
-		require_once WP_PLUGIN_DIR . '/borncreative_wc-cart-view/views/borncreative_wc-cart-view-admin-view.php';
-	}
+    // include admin view
+    if (file_exists(WP_PLUGIN_DIR . '/borncreative_wc-cart-view/views/borncreative_wc-cart-view-admin-view.php')) {
+        require_once WP_PLUGIN_DIR . '/borncreative_wc-cart-view/views/borncreative_wc-cart-view-admin-view.php';
+    }
 }
 
 function borncreative_wc_get_carts_information()
 {
-	global $wpdb;
-	// carts are in woocommerce_sessions table
-	// first we need to get that whole table.
+    global $wpdb;
+    // carts are in woocommerce_sessions table
+    // first we need to get that whole table.
 
-	echo '<h2>Carts</h2>';
-	$table_name = $wpdb->prefix . 'woocommerce_sessions';
-	$result = $wpdb->get_results("SELECT * FROM $table_name");
+    echo '<h2>Carts</h2>';
+    $table_name = $wpdb->prefix . 'woocommerce_sessions';
+    $result = $wpdb->get_results("SELECT * FROM $table_name");
 
-	echo "<table>";
-	echo "<tr><td>Product ID</td><td>Product Name</td><td>Quantity</td><td>Total</td></tr>";
-	foreach ($result as $session) {
-		// do we have actual cart datas?
-		$datas = unserialize($session->session_value);
-		if (!empty($datas)) {
-			$array = unserialize($datas['cart']);
+    echo "<table>";
+    echo "<tr><td>Product ID</td><td>Product Name</td><td>Quantity</td><td>Total</td></tr>";
+    foreach ($result as $session) {
+        // do we have actual cart datas?
+        $datas = unserialize($session->session_value);
+        if (!empty($datas)) {
+            $array = unserialize($datas['cart']);
 
-			if (!empty($array)) {
-				//     // we have a cart
+            if (!empty($array)) {
+                //     // we have a cart
 
-				$temp = array_keys($array);
-				$key = $temp[0];
-				$cart = $array[$key];
-				$product = wc_get_product($cart['product_id']);
-				echo "<tr>";
-				echo "<td>" . $cart['product_id'] . "</td>";
-				echo "<td>" . $product->get_name() . "</td>";
-				echo "<td>" . $cart['quantity'] . "</td>";
-				echo "<td>" . $cart['line_total'] . "</td>";
-				echo "</tr>";
+                $temp = array_keys($array);
+                $key = $temp[0];
+                $cart = $array[$key];
+                $product = wc_get_product($cart['product_id']);
+                echo "<tr>";
+                echo "<td>" . $cart['product_id'] . "</td>";
+                echo "<td>". $product->get_name() . "</td>";
+                echo "<td>" . $cart['quantity'] . "</td>";
+                echo "<td>" . $cart['line_total'] . "</td>";
+                echo "</tr>";
 
 
 
-				// echo "<tr><td>" . $product->get_name() . "</td><td>" . $cart[$key_name[0]]['quantity'] . "</td></tr>";
+                // echo "<tr><td>" . $product->get_name() . "</td><td>" . $cart[$key_name[0]]['quantity'] . "</td></tr>";
 
-				// $product = wc_get_product( $cart[$key_name[0]]);
+                // $product = wc_get_product( $cart[$key_name[0]]);
 
-			}
-		}
-	}
-	echo "</table>";
+            }
+        }
+    }
+    echo "</table>";
 }
